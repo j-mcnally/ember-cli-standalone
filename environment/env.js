cd515ea37c641f8c3c3a43d8495e4e1e092c3fd2
Justin McNally <justin@kohactive.com>
2014-09-04 23:44:35 -0500

    remove node modules

---
+++ environment/env.js
@@ -0,0 +1,3 @@
+window.{{NAMESPACE}}ENV = {{ENV}};
+window.EmberENV = window.{{NAMESPACE}}ENV.EmberENV;
+window.ENV = window.{{NAMESPACE}}ENV; // Legacy support remove when all plugins are up to date.
